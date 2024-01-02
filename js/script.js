abee0b4581c9260ee0f9ae02727a3c241bb5c544
Iván Tortosa <152330066+LiquidRay-1@users.noreply.github.com>
2024-01-02 21:16:27 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,31 +5,32 @@ let isDeleting = false;
 let speed = 200; // Velocidad de escritura y borrado
 
 const platformElement = document.getElementById('platform');
+const constantChar = "#";
 
 function type() {
     const currentWord = words[wordIndex];
 
-    if (!isDeleting && letterIndex <= currentWord.length) {
-        platformElement.textContent = currentWord.substring(0, letterIndex);
-        letterIndex++;
-        speed = 200; // Velocidad de escritura
-    } else if (isDeleting && letterIndex >= 0) {
-        platformElement.textContent = currentWord.substring(0, letterIndex);
+    let displayedText = constantChar + currentWord.substring(0, letterIndex);
+
+    if (isDeleting) {
+        displayedText = constantChar + currentWord.substring(0, letterIndex);
         letterIndex--;
-        speed = 100; // Velocidad de borrado
+    } else {
+        letterIndex++;
     }
 
-    if (letterIndex === currentWord.length + 1) {
+    if (letterIndex > currentWord.length) {
         isDeleting = true;
         speed = 1000; // Tiempo de pausa antes de iniciar la siguiente palabra
     }
 
-    if (isDeleting && letterIndex === 0) {
+    if (letterIndex === 0) {
         isDeleting = false;
         wordIndex = (wordIndex + 1) % words.length;
         speed = 500; // Tiempo de pausa entre palabras
     }
 
+    platformElement.textContent = displayedText;
     setTimeout(type, speed);
 }
 
